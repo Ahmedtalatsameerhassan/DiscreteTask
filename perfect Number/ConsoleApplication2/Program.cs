8ed1bc96514baa261c4efe85c2953d479336e138
Ahmedtalatsameerhassan <121636900+Ahmedtalatsameerhassan@users.noreply.github.com>
2022-12-29 21:00:49 +0200

    Add files via upload

---
+++ perfect Number/ConsoleApplication2/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n1, n2, r;
+            Console.WriteLine("enter the frist number :");
+            n1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("enter the last number :");
+            n2 = int.Parse(Console.ReadLine());
+            Console.WriteLine("the perfect number is :");
+            for (int i = n1; i <= n2; i++)
+            {
+                int factor = 0;
+                for (int z = 1; z <= n2; z++)
+                {
+                    if ((i % z == 0) & (i != 1))
+                    {
+                        if (z == i)
+                        {
+                            continue;
+
+                        }
+                        else
+                            factor += z;
+                    }
+                    else
+                        continue;
+
+                }
+                if (factor == i)
+                {
+                    Console.WriteLine(i);
+                }
+                else
+                    factor = 0;
+
+
+            }
+
+
+
+        }
+    }
+}
---
+++ prime Number/ConsoleApplication1/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool flag = true;
+            Console.WriteLine("Please Enter Frist Number:");
+            int num = int.Parse(Console.ReadLine());
+            Console.WriteLine("Please Enter Other Number:");
+            int num1 = int.Parse(Console.ReadLine());
+
+            if (num < 2)
+                num = 2;
+
+            Console.WriteLine("The prime number is :");
+            for (int a = num; a <= num1; a++)
+            {
+                for (int n = 2; n <= num1; n++)
+                {
+                    if (a != n && a % n == 0)
+                    {
+                        flag = false;
+                        break;
+                    }
+
+                }
+                if (flag)
+                {
+                    Console.WriteLine("\t" + a);
+                }
+                flag = true;
+
+
+            }
+        }
+    }
+}
